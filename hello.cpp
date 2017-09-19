08e506f321aa46b5b89a1e776c9ff016fd59d7d2
yusongc <32110941+yusongc@users.noreply.github.com>
2017-09-19 15:47:47 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,5 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "My name is Ethan Cai and my student ID is 301269793"<< endl; //cout my name is Ethan with id 301269793
 }
